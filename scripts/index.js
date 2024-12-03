837b9a7bc5d9b971f8911b5a8b649d4468782dc3
mac <mac@macs-MacBook-Pro-3.local>
2024-12-03 10:48:02 -0500

    slack conversion

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
 const PROJECT_NAME = 'Calendly'
+const SENSITIVE_CHANNELS = [
+]
 
 let previousState = {};
 let initialState = true
@@ -7,21 +9,31 @@ let initialState = true
   while(true) {
     try {
       const currentState = {}
-      const chatItems = document.querySelectorAll("div[data-shortcut-context='chat-list'] div[role='group'] [data-tid='chat-list-item'] > div.chatListItem_mainMedia")
+      const chatItems = document.querySelectorAll("div.c-virtual_list__scroll_container > div.p-channel_sidebar__static_list__item div.p-channel-sidebar__channel--unread")
       for(let chatItem of chatItems) {
+        const dataItemKey = chatItem.getAttribute('data-item-key')
+        if(dataItemKey === 'pagesListSpacer' ||
+          dataItemKey.endsWith('-huddles') ||
+          dataItemKey.startsWith('sectionHeading-')
+        ) {
+          continue
+        }
+          
         try {
-          const header = chatItem.querySelector('.chatListItem_mainMedia_header').textContent
-          const timestamp = chatItem.querySelector('.chatListItem_mainMedia_timeStamp').textContent
-          const preview = chatItem.querySelector('.chatListItem_mainMedia_preview').textContent
-          currentState[header] = { timestamp, preview }
+          const header = chatItem.querySelector('.p-channel-sidebar__name').textContent
+          const badgeElement = chatItem.querySelector('.p-channel-sidebar__badge').textContent
+          let badge = null
+          if(badgeElement) badge = badgeElement.textContent
+          else if(SENSITIVE_CHANNELS.includes(header)) badge = 'channel'
+          if(badge)
+            currentState[header] = { badge }
         } catch(err) {}
       }
       if(!initialState) {
         let text = ''
         for(let header in currentState) {
-          if(currentState[header].preview !== previousState[header].preview) {
-            text += `*${header} @ ${PROJECT_NAME}* sent message!
-  \`${currentState[header].preview}\`\n`
+          if(currentState[header].badge !== previousState[header].badge) {
+            text += `*${header} @ ${PROJECT_NAME}* sent ${badge} message on Slack\n`
           }
         }
         if(text.length > 0) {
